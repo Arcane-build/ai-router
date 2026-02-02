710765443396d6d746540382bea90c0465172627
chainstacker <jatinhemantpawar@gmail.com>
2026-02-02 20:15:57 +0530

    Switch from HashRouter to BrowserRouter for better Vercel compatibility

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 // import Login from "./pages/Login";
 import Profile from "./pages/Profile";
@@ -18,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           {/* Public routes */}
           {/* <Route path="/login" element={<Login />} /> */}
@@ -44,7 +43,7 @@ const App = () => (
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
 );
